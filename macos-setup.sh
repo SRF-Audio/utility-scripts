1520d8fe916c19570f137c2ae49648536bf94d06
Stephen Froeber <srfaudioproductions@gmail.com>
2024-01-29 19:26:37 -0600

    Create macos-setup.sh
    Initial script

---
+++ macos-setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------
+# 
+# START HERE if you're on MacOS
+# MacOS Development Environment Setup Script
+#
+# Prior to running this script, ensure the following commands have been run:
+# chmod +x setup_mac.sh && ./setup_mac.sh
+# ------------------------------------------------------------------------------
+
+# Check if Homebrew is installed, if not, install it
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew could not be found, installing..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Install necessary tools
+brew install git xclip
+
+# Install compilers and build tools
+brew install gcc
+
+# Install software tools
+brew install python
+echo "alias python='python3'" >> ~/.zshrc
+source ~/.zshrc
+python3 -m ensurepip --upgrade
+echo "alias pip='pip3'" >> ~/.zshrc
+source ~/.zshrc
+
+brew install go kustomize helm kubectl skaffold istioctl derailed/k9s/k9s
+
